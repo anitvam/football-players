23d540f315d9ee432544ca140c5fc6761ca65a62
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2024-01-10 10:03:22 +0100

    fix: refactor of project structure

--- setup.py
+++ setup.py
@@ -1,66 +1,9 @@
 from setuptools import setup, find_packages
 import pathlib
-import subprocess
-import distutils.cmd
-
-# current directory
-
-here = pathlib.Path(__file__).parent.resolve()
-
-version_file = here / 'VERSION'
-
-
-def format_git_describe_version(version):
-    if '-' in version:
-        splitted = version.split('-')
-        tag = splitted[0]
-        index = f"dev{splitted[1]}"
-        return f"{tag}.{index}"
-    else:
-        return version
-
-
-def get_version_from_git():
-    try:
-        process = subprocess.run(["git", "describe"], cwd=str(here), check=True, capture_output=True)
-        version = process.stdout.decode('utf-8').strip()
-        version = format_git_describe_version(version)
-        with version_file.open('w') as f:
-            f.write(version)
-        return version
-    except subprocess.CalledProcessError:
-        if version_file.exists():
-            return version_file.read_text().strip()
-        else:
-            return 'v0.0.3'
-
-
-version = get_version_from_git()
-
-
-print(f"Detected version {version} from git describe")
-
-
-class GetVersionCommand(distutils.cmd.Command):
-    """A custom command to get the current project version inferred from git describe."""
-
-    description = 'gets the project version from git describe'
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        print(version)
-
-
 
 setup(
-    name='anitvam-fp',  # Required
-    version=version,
+    name='fopl',  # Required
+    version='0.0.2',
     description='Application example showing Internazionale football players using tkinter and cURL',
     license='Apache 2.0 License',
     author='Martina Baiardi',
@@ -68,4 +11,9 @@ setup(
     packages=find_packages(),  # Required
     include_package_data=True,
     platforms = "Independant",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
 )
