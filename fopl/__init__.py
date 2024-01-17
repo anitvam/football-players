8c183751b656ee868def3ed827ef7b6e5e827ea9
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2024-01-17 14:35:09 +0100

    feat: addition of utf-8 encoding and change soccer team displayed

--- fopl/__init__.py
+++ fopl/__init__.py
@@ -5,17 +5,17 @@ from tkinter import ttk
 
 
 def main():
-    command = 'curl https://www.besoccer.com/team/squad/internazionale'
+    command = 'curl https://www.besoccer.com/team/squad/pergolettese'
     res = os.popen(command).read()
     players = re.findall('<a href=.* data-cy=\"player\">(.*)<\/a>', res)
 
     window = Tk()
-    window.title("Internazionale Squad")
+    window.title("Pergolettese Soccer Squad")
     frm = ttk.Frame(window, padding=100)
     frm.grid()
-    ttk.Label(frm, text="Hello, Internazionale soccer team players are: ").grid(column=0, row=0)
+    ttk.Label(frm, text="Pergolettese soccer team players are: ").grid(column=0, row=0)
 
     for idp, player in enumerate(players):
-       ttk.Label(frm, text=player).grid(column=0, row=idp+1)
+       ttk.Label(frm, text=player.encode('utf8')).grid(column=0, row=idp+1)
 
     window.mainloop()
